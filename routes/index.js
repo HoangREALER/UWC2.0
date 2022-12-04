33c117856ff579be598f6761b32273eeb64edc36
HoangREALER <hoangle1742002@google.com>
2022-12-05 00:58:55 +0700

    modified:   routes/index.js 	controllers/EmployeeCotroller.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { loginView } = require('../controllers/LoginController');
 const { mapView } = require('../controllers/MapController');
+const { employeeList } = require('../controllers/EmployeeController')
 const router = express.Router();
 
 
 router.get('/login', loginView);
 router.get('/map', mapView);
+router.get('/employee', employeeList);
 
 
 module.exports = () => {
