c97be0b487abefec38fcb22da64a534453f7e5d5
hoangREALER <hoangle1742002@gmail.com>
2022-12-06 08:49:19 +0700

    Final final commit

---
+++ views/layout/menubar.js
@@ -0,0 +1,44 @@
+<section id="menu">
+        <div class="logo">
+            <img src="/assets/img/logo.png"></img>
+            <h2>UWC2.0</h2>
+        </div>
+        <div class="items">
+            <a  class="active" href="#">
+                <li>
+                    <i class="fas fa-chart-pie"></i>
+                    <span href="#">Dashboard</span>
+                </li>
+            </a>
+            <a href="#">
+                <li>
+                    <i class="fa-solid fa-person-digging"></i>
+                    <span href="#">Employees</span>
+                </li>
+            </a>
+            <a href="#">
+                <li>
+                    <i class="fa-solid fa-truck"></i>
+                    <span href="#">Vehicles</span>
+                </li>
+            </a>
+            <a href="#">
+                <li>
+                    <i class="fa-solid fa-location-pin"></i>
+                    <span href="#">Checkpoints</span>
+                </li>
+            </a>
+            <a href="#">
+                <li>
+                    <i class="fa-solid fa-map-location-dot"></i>
+                    <span href="#">Map</span>
+                </li>
+            </a>
+            <a href="#">
+                <li>
+                    <i class="fa-solid fa-chart-line"></i>
+                    <span href="#">Statistics</span>
+                </li>
+            </a>
+        </div>
+</section>
